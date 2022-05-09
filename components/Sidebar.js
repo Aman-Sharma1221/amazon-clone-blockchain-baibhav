a5c93d34369fc83e3a4e81d5cf1ece4599e61d87
baibhav <baibhavtiwari37@gmail.com>
2022-05-09 19:12:29 +0530

    yes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,9 +10,6 @@ import { ConnectButton } from 'web3uikit'
 import { AiOutlineHistory } from 'react-icons/ai'
 import Link from 'next/link'
 
-const isAuthenticated = true
-const username = 'baibhav'
-
 const Sidebar = () => {
     const styles = {
         container: `h-full w-[300px] flex flex-col bg-[#fff] static`,
@@ -20,69 +17,82 @@ const Sidebar = () => {
         profilePicContainer: `flex  rounded-xl items-center justify-center w-full h-full mb-5`,
         profilePic: `rounded-3xl object-cover`,
         welcome: ` text-md mb-2 font-bold text-2xl text-white`,
+        walletAddress: `text-xl flex w-full justify-center font-extrabold mb-4`,
+        menu: `flex flex-col w-full h-full px-10 gap-10`,
+        menuItem: `flex items-center text-lg font-bold cursor-pointer gap-2`,
+        amazonLogo: `mr-4 flex object-cover`,
+        companyName: `text-lg font-bold flex flex-1 pl-10 items-center mt-[20px]`,
         usernameInput: `bg-transparent border-white border-2 rounded-lg w-[80%] py-2 px-4 text-lg mt-[20px] placeholder:text-white focus:outline-none flex justify-center items-center text-white`,
         username: `flex items-center w-full justify-center`,
         setNickname: `text-lg font-bold flex flex-1 items-center mt-[20px] mb-[20px] text-white`,
-        companyName: `text-lg font-bold flex flex-1 pl-10 items-center mt-[20px]`,
     }
+
+    const {
+        isAuthenticated,
+        buyTokens,
+        getBalance,
+        nickname,
+        setNickname,
+        username,
+        handleSetUsername,
+    } = useContext(AmazonContext)
+
     return (
         <div className={styles.container}>
             <div className={styles.profile}>
-                {
-                    isAuthenticated && (
-                        <>
-                            <div className={styles.profilePicContainer}>
-                                <Image
-                                    src={`https://avatars.dicebear.com/api/pixel-art/${username}.svg`}
-                                    alt='profile'
-                                    className={styles.profilePic}
-                                    height={100}
-                                    width={100}
-                                />
-                            </div>
-                            {!username ? (
-                                <>
-                                    <div className={styles.username}>
-                                        <input
-                                            type="text"
-                                            placeholder="Username"
-                                            className={styles.usernameInput}
-                                        // value={nickname}
-                                        // onChange={e=> setNickname(e.target.value)} 
-                                        />
-                                    </div>
-                                    <button
-                                        className={styles.setNickname}
-                                    // onClick={handleSetUsername}
-                                    >
-                                        set Nickname
-                                    </button>
-                                </>
-                            ) : (
-                                <div>
-                                    <div className={styles.welcome}>Welcome {username}</div>
+                {isAuthenticated && (
+                    <>
+                        <div className={styles.profilePicContainer}>
+                            <Image
+                                src={`https://avatars.dicebear.com/api/pixel-art/${username}.svg`}
+                                alt='profile'
+                                className={styles.profilePic}
+                                height={100}
+                                width={100}
+                            />
+                        </div>
+                        {!username ? (
+                            <>
+                                <div className={styles.username}>
+                                    <input
+                                        type='text'
+                                        placeholder='Username....'
+                                        className={styles.usernameInput}
+                                        value={nickname}
+                                        onChange={e => setNickname(e.target.value)}
+                                    />
                                 </div>
-
-                            )}
-                        </>
-                    )}
-                <div className={styles.ConnectButton}>
+                                <button
+                                    className={styles.setNickname}
+                                    onClick={handleSetUsername}
+                                >
+                                    Set Nickname
+                                </button>
+                            </>
+                        ) : (
+                            <div>
+                                <div className={styles.welcome}>Wecome {username}</div>
+                            </div>
+                        )}
+                    </>
+                )}
+                <div className={styles.connectButton}>
                     <ConnectButton />
                 </div>
             </div>
             <div className={styles.menu}>
-                <link href='/'>
+                <Link href='/'>
                     <div className={styles.menuItem}>
                         <Image
                             src={logo}
                             height={30}
                             width={30}
-                            className={styles.amazonlogo}
+                            className={styles.amazonLogo}
                         />
                         My Amazon
-                        <br /> board
+                        <br /> Board
                     </div>
-                </link>
+                </Link>
                 <div className={styles.menuItem}>
                     <FaBox />
                     Collections
@@ -93,14 +103,14 @@ const Sidebar = () => {
                 </div>
                 <div className={styles.menuItem}>
                     <BsFillPersonFill />
-                    Saved
+                    Profile
                 </div>
-                <link href='/history'>
+                <Link href='/history'>
                     <div className={styles.menuItem}>
                         <AiOutlineHistory />
                         Transaction History
                     </div>
-                </link>
+                </Link>
             </div>
             <div className={styles.companyName}>
                 <Image src={logoFull} alt='amazon' height={100} width={100} />
@@ -108,4 +118,5 @@ const Sidebar = () => {
         </div>
     )
 }
+
 export default Sidebar
